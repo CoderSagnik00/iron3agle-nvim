f063a90968377b4b58fb2d796d3b80a250950435
RK800 <157528393+CoderSagnik00@users.noreply.github.com>
2025-06-13 20:19:29 +0000

    Update telescope.lua
    Fixed the problem where telescope ui trying to load before telescope loads

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -41,24 +41,27 @@ return {
         sorting_strategy = "ascending",
         winblend = 15,
       },
-      extensions = {
-        ["ui-select"] = require("telescope.themes").get_cursor({
-          winblend = 15,
-        }),
-      },
     },
     config = function(_, opts)
-      require("telescope").setup(opts)
-      require("telescope").load_extension("ui-select")
+      local telescope = require("telescope")
+      local themes = require("telescope.themes")
+
+      -- Add extension config here (deferred until telescope is loaded)
+      opts.extensions = {
+        ["ui-select"] = themes.get_cursor({
+          winblend = 15,
+        }),
+      }
+
+      telescope.setup(opts)
+      telescope.load_extension("ui-select")
     end,
   },
   {
-    {
-      "rmagatti/session-lens",
-      dependencies = { "nvim-telescope/telescope.nvim" },
-      config = function()
-        require("telescope").load_extension("session-lens")
-      end,
-    },
+    "rmagatti/session-lens",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    config = function()
+      require("telescope").load_extension("session-lens")
+    end,
   },
 }
